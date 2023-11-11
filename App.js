fc2266130023180970ad1b21b513732e3b158a6a
JhonyVS <never_more_wars@yahoo.com>
2023-11-11 18:58:53 -0400

    tabla produccion fabrica

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Table from './src/components/table';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hola putasos</Text>
+        <View style={styles.titulo}>
+          <Text style={styles.textoTitulo}>PRODUCCION DIARIA DE LA FABRICA</Text>
+        </View>
+        <Table/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +18,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  titulo: {
+    flex: 0.2, 
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 120,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  textoTitulo: {
+    flex: 0,
+    backgroundColor: '#fff',
+    fontWeight: 'bold',
   },
 });
